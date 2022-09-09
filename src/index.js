5dda74a4faaf9f3115d9e75df3c1fe9d2d886b17
asmaasifchippa786 <asmaasifchippa786@gmail.com>
2022-09-09 20:18:18 +0500

    1st

---
+++ src/index.js
@@ -0,0 +1,244 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Css from "./index.css";
+
+ReactDOM.render(
+  <>
+      <div className="banner">
+      <header>
+        <a href="" className="logo">M Anas Asif</a>
+        <div className="toggle"></div>
+      </header>
+      <img src="https://website-demo-jecso.netlify.app/bg.jpg" alt="" />
+      <div
+        className="content"
+        data-0-top="opacity:0;left:100px;"
+        data-200-top="opacity:1;left:0px;"
+      >
+        <h2>I am Web Designer</h2>
+        <p>
+           I am a Full Expert in HTML5, CSS3, Bootstrap(3,4,5), Javascript, Adobe Illustrator, Adobe Photoshop, Adobe XD, Adobe Premiere Pro, Jungle Scout, Helium 10, Adobe After Effect, and Adobe Audition. You will be Full Satisfied with My work. I will be searching Best Keywords for amazon. Join Me Now.
+        </p>
+      </div>
+    </div>
+
+    <section className="about">
+      <div className="contentBx">
+        <h2
+          className="heading"
+          data-center-center="opacity:1;left:0;"
+          data-0-bottom="opacity:0;left:500px;"
+        >
+          About Me
+        </h2>
+        <p
+          className="text"
+          data-center-center="opacity:1;left:0;"
+          data-0-bottom="opacity:0;left:-500px;"
+        >
+         Hi! My Actual Name is Anas Asif.
+          I am a Graphic Designer, Web Designer,
+           and Virtual Assistant on Amazon,
+            eBay, and Alibaba.
+           I am a Full Expert in HTML5, CSS3, Bootstrap(3,4,5), Javascript, Adobe Illustrator, Adobe Photoshop, Adobe XD, Adobe Premiere Pro, Jungle Scout, Helium 10, Adobe After Effect, and Adobe Audition. You will be Full Satisfied with My work. I will be searching Best Keywords for amazon. Join Me Now. !! Thanks !! <br /><br />
+           I am a multi-stack developer. Also a good communicator. And my first preference is good quality. I am here to entertain those people who want premium quality products at appropriate rates. I have good experience in designing and developing desktop/mobile & web applications. I'll deliver much more than your expectations. You'll enjoy my company.
+        </p>
+      </div>
+      <div
+        className="imgbx"
+        data-center-center="opacity:1;left:0;"
+        data-0-bottom="opacity:0;left:200px;"
+      ></div>
+    </section>
+
+    <section className="services">
+      <h2
+        className="heading"
+        data-center-center="opacity:1;left:0;"
+        data-0-bottom="opacity:0;left:500px;"
+      >
+        Our Services
+      </h2>
+      <p
+        className="text"
+        data-center-center="opacity:1;right:0;"
+        data-0-bottom="opacity:1;right:500px;"
+      >
+       I am a Fast, Responsible, Certified and Experienced Web Designer and Graphic designer. Working as a Professional Web Designer since 2021 and completed over 50 web project at various marketplaces. My goal is to provide best service to my clients, If you want a high quality & professional work then just simply inbox me. Looking forward to work with you.
+      </p>
+      <div className="container">
+        <div
+          className="serviceBx"
+          data-center-center="opacity:1;left:0;"
+          data-0-bottom="opacity:0;left:-400px;"
+        >
+          <div>
+            <img src="https://website-demo-jecso.netlify.app/icon1.png" alt="" />
+            <h2>Design</h2>
+          </div>
+        </div>
+        <div
+          className="serviceBx"
+          data-center-center="opacity:1;bottom:0;"
+          data-0-bottom="opacity:0;bottom:-400px;"
+        >
+          <div>
+            <img src="https://website-demo-jecso.netlify.app/icon2.png" alt="" />
+            <h2>Development</h2>
+          </div>
+        </div>
+        <div
+          className="serviceBx"
+          data-center-center="opacity:1;right:0;"
+          data-0-bottom="opacity:0;right:-400px;"
+        >
+          <div>
+            <img src="https://website-demo-jecso.netlify.app/icon3.png" alt="" />
+            <h2>Marketing</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  
+ <section className="client">
+      <h2
+        className="heading"
+        data-center-center="opacity:1;top:0;"
+        data-0-bottom="opacity:0;top:-100px;"
+      >
+        Our Sites
+      </h2>
+      <p
+        className="text"
+        data-center-center="opacity:1;bottom:0;"
+        data-0-bottom="opacity:0;bottom:-100px;"
+      >
+        I am avalable in all social accounts. like instagram, youtube, likedin, GitHub, twitter, google, netlify, Facebook, dribble, and beahance. 
+      </p>
+      <div className="imgbx">
+        <img
+          src="https://website-demo-jecso.netlify.app/brand1.png"
+          alt=""
+          data-center-center="top:0;"
+          data-0-bottom="top:100px;"
+        />
+        <img
+          src="https://website-demo-jecso.netlify.app/brand2.png"
+          alt=""
+          data-center-center="top:0;"
+          data-0-bottom="top:150px;"
+        />
+        <img
+          src="https://website-demo-jecso.netlify.app/brand3.png"
+          alt=""
+          data-center-center="top:0;"
+          data-0-bottom="top:200px;"
+        />
+        <img
+          src="https://website-demo-jecso.netlify.app/brand4.png"
+          alt=""
+          data-center-center="top:0;"
+          data-0-bottom="top:250px;"
+        />
+      </div>
+    </section>
+
+    <section className="testimonials">
+      <h2 className="heading">What Our Client says</h2>
+      <div className="flex">
+        <div className="container">
+          <div
+            className="contentbx"
+            data-center-center="opacity:1;left:0;"
+            data-0-bottom="opacity:0;left:-400px;"
+          >
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+                explicabo molestiae amet architecto a aut dolorum unde porro
+                nisi ab reiciendis, minima dignissimos numquam nam doloremque
+                sapiente consequuntur fugiat quia!
+              </p>
+              <br />
+              <h3>Someone Famous</h3>
+            </div>
+          </div>
+
+          <div
+            className="contentbx"
+            data-center-center="opacity:1;right:0;"
+            data-0-bottom="opacity:0;right:-400px;"
+          >
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+                explicabo molestiae amet architecto a aut dolorum unde porro
+                nisi ab reiciendis, minima dignissimos numquam nam doloremque
+                sapiente consequuntur fugiat quia!
+              </p>
+              <br />
+              <h3>Someone Famous</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section className="contact">
+      <div
+        data-center-center="opacity:1;left:0;"
+        data-0-bottom="opacity:0;left:-400px;"
+      >
+        <h2 className="heading">Contant us</h2>
+        <p className="text">
+          please contact me, if any work for me, contact me on instagram, youtube, likedin, GitHub, twitter, google, netlify, Facebook, dribble, and beahance. 
+        </p>
+      </div>
+    </section>
+    <section className="about">
+      <div className="contentBx  redbg">
+        <div
+          className="form"
+          data-center-center="opacity:1;left:0;"
+          data-0-bottom="opacity:0;left:-400px;"
+        >
+          <div className="inputbx">
+            <input type="text" placeholder="Full Name" />
+          </div>
+          <div className="inputbx">
+            <input type="text" placeholder="Email Adress" />
+          </div>
+          <div className="inputbx">
+            <input type="text" placeholder="Mobile no." />
+          </div>
+          <div className="inputbx">
+            <textarea placeholder="Write Your message Here"></textarea>
+          </div>
+          <div className="inputbx">
+            <input type="submit" value="Send" />
+          </div>
+        </div>
+      </div>
+      <div className="imgbx2"></div>
+    </section>
+
+    <section className="footer">
+      <p className="text">Design & Developed By Anas Asif</p>
+      <ul>
+        <p className="text">Follow On:</p>
+        <li>
+          <a href="#"><img src="https://website-demo-jecso.netlify.app/facebook.png" alt="" /></a>
+        </li>
+        <li>
+          <a href="#"><img src="https://website-demo-jecso.netlify.app/twitter.png" alt="" /></a>
+        </li>
+        <li>
+          <a href="#"><img src="https://website-demo-jecso.netlify.app/linkedin.png" alt="" /></a>
+        </li>
+      </ul>
+    </section>
+  </>,
+  document.querySelector("#root")
+);
+
